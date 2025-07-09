b9e6315aad34f63772f473a83408500f785754e8
Xavier Prot <prot.xavier@gmail.com>
2025-07-09 16:36:54 +0200

    add property isAdmin to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,9 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'isAdmin' => 'boolean',
         ];
     }
+
+
 }
