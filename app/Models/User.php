2d516af90906bbb0ceec54d68f60776286670afc
Xavier Prot <prot.xavier@gmail.com>
2025-07-09 16:40:29 +0200

    add user booking relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use App\Models\Booking;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -47,5 +50,8 @@ class User extends Authenticatable
         ];
     }
 
-
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
