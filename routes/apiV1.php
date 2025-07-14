36efb07271ccc23a312294fc2fca4e1e73315d20
Xavier Prot <prot.xavier@gmail.com>
2025-07-14 14:53:41 +0200

    fix routing for service,booking and slots endpoints

--- routes/apiV1.php
+++ routes/apiV1.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\BookingController;
+use App\Http\Controllers\Api\V1\ServiceController;
+use App\Http\Controllers\Api\V1\SlotsController;
 
-Route::name('api.')->group(function () {
-    Route::get('services', function () {
-        return 'CIAO';
-    });
+Route::name('api.')->middleware('auth:sanctum')->group(function () {
+    Route::get('services/{service}/slots/{date}', [SlotsController::class,'index'])->name('services.slots');
+    Route::apiResource('services', ServiceController::class);
+    Route::apiResource('bookings', BookingController::class);
 });
+
