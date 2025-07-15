bcbf29a72f7ac27017b59e3f3edf2e404ae8eb70
Xavier Prot <prot.xavier@gmail.com>
2025-07-15 16:11:48 +0200

    fix api.service.index endpoint can be reached by guest

--- routes/apiV1.php
+++ routes/apiV1.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->middleware('auth:sanctum')->group(function () {
     Route::get('services/{service}/slots/{date}', [SlotsController::class, 'index'])->name('services.slots');
-    Route::apiResource('services', ServiceController::class);
+    Route::apiResource('services', ServiceController::class)->except('index');
     Route::apiResource('bookings', BookingController::class);
+});
+
+Route::name('api.')->group(function () {
+    Route::apiResource('services', ServiceController::class)->only('index');
 });
